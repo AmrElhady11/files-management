90e303a041b65eb093041934ebebb37f5fce760e
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-10-01 15:06:30 +0300

    add api of getdocumenturl

--- src/main/java/com/filesmanagement/controller/DocumentController.java
+++ src/main/java/com/filesmanagement/controller/DocumentController.java
@@ -1,10 +1,19 @@
 package com.filesmanagement.controller;
 
+import com.filesmanagement.entity.Document;
+import com.filesmanagement.response.UrlResponse;
 import com.filesmanagement.service.DocumentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,5 +24,12 @@ public class DocumentController {
     public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) {
         documentService.uploadDocument(file);
         return ResponseEntity.ok().body("Documents uploaded successfully");
+    }
+
+    @GetMapping("/getAllDocument/{userId}")
+    public ResponseEntity<List<UrlResponse>> getAllDocument(@PathVariable int userId) {
+    List<UrlResponse> responses = new ArrayList<>();
+    documentService.getDocumentUrl(userId).forEach(url -> responses.add(new UrlResponse(url)));
+    return ResponseEntity.ok(responses);
     }
 }

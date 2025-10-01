9e962a09ab74386bdd91cc2b898b56e69691ac56
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-10-01 15:05:30 +0300

    add findbyuserId method

--- src/main/java/com/filesmanagement/repository/DocumentRepository.java
+++ src/main/java/com/filesmanagement/repository/DocumentRepository.java
@@ -3,5 +3,8 @@ package com.filesmanagement.repository;
 import com.filesmanagement.entity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DocumentRepository extends JpaRepository<Document, Integer> {
+    List<Document> findByUserId(int userId);
 }
